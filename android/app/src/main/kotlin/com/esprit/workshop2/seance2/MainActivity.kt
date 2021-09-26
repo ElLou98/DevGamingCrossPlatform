a46290646e869100d39125d653e24c3cdbe62188
Louay Yahyaoui <louay.yahyaoui1@esprit.tn>
2021-09-26 17:33:03 +0100

    Ce qui a ete fait en cours

---
+++ android/app/src/main/kotlin/com/esprit/workshop2/seance2/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.esprit.workshop2.seance2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/home.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:seance2/product_info.dart';
+
+class Home extends StatelessWidget {
+  const Home ({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("G-Store ESPRIT"),
+      ),
+      body: Column(
+        children: [
+          ProductInfo("assets/images/dmc5.jpg", "Devil May Cry 5", 200),
+          ProductInfo("assets/images/re8.jpg", "Resident Evil 8", 200),
+          ProductInfo("assets/images/nfs.jpg", "Need For speed", 100),
+          ProductInfo("assets/images/rdr2.jpg", "Red Dead Redemption 2", 150),
+          ProductInfo("assets/images/fifa.jpg", "FIFA 22", 100),
+        ],
+      ),
+    );
+  }
+}
